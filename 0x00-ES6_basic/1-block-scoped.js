be1027c7994202005c6f730bcb5e0c57b6745b87
nuel07 <yehanganep@gmail.com>
2022-04-05 07:22:29 -0700

    task 1 eslint corrections

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,17 @@
 /**
  * function with variables that can't be overwritten
- * @return []
+ * @param {string} trueOrFalse
+ * @return {list}
  */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
+  /* eslint-disable no-unused-vars */
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+  /* eslint-disable no-unused-vars */
 
-    if (trueOrFalse) {
-	var task = true;
-	var task2 = false;
-    }
-
-    return [task, task2];
+  return [task, task2];
 }
