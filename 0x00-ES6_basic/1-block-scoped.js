0b8e88230fd1278170bf06aca9ce85cdef1665ff
nuel07 <yehanganep@gmail.com>
2022-04-05 07:07:44 -0700

    task 1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+/**
+ * function with variables that can't be overwritten
+ * @return []
+ */
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+
+    if (trueOrFalse) {
+	var task = true;
+	var task2 = false;
+    }
+
+    return [task, task2];
+}
