f4bb27e9d77eeb7af58cb2f0765d9fbfd40a92ae
nuel07 <yehanganep@gmail.com>
2022-04-05 06:14:27 -0700

    ESlint corrections

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,25 @@
-export function taskFirst(){
-    const task = 'I prefer const when I can';
-    return task;
+/**
+ * function that uses const for variable declaration
+ * @return {string} The variable value
+ */
+export function taskFirst() {
+  const task = 'I prefer const when I can';
+  return task;
 }
-
-export function getLast(){
-    return 'is okay';
+/**
+ * functions with no varible
+ * @return {string}
+ */
+export function getLast() {
+  return 'is okay';
 }
+/**
+ * function that uses let for variable declaration
+ * @return {string} The variable value
+ */
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-export function taskNext(){
-    let combination = 'But sometimes let';
-    combination += getLast();
-
-    return combination;
+  return combination;
 }
