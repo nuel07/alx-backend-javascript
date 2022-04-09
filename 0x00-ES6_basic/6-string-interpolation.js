c0ffda03f8a7911cf4e29fcc126df5e1aee8cb7f
nuel07 <yehanganep@gmail.com>
2022-04-08 23:32:42 -0700

    final typo fix

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -11,5 +11,5 @@ export default function getSanFranciscoDescription() {
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income
-of ${budget.income}. As of 2015, San Francisco proper had a GDP fo ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
