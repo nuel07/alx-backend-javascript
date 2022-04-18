5f55286674dfc0453ceb498da31101724c988b86
nuel07 <yehanganep@gmail.com>
2022-04-18 11:51:33 -0700

    task 3 lint fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,7 @@
  * function that returns sum of ids
  * @param{list} studentList
  */
-export default function(studentList) {
-    const idSum = studentList.reduce((theSum, student) => theSum + student.id, 0);
-    return idSum;
+export default function (studentList) {
+  const idSum = studentList.reduce((theSum, student) => theSum + student.id, 0);
+  return idSum;
 }
