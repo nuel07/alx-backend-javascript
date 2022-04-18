fc2f0e2a15fd63829c1504e4ce19ef5c921f3aaa
nuel07 <yehanganep@gmail.com>
2022-04-18 11:17:01 -0700

    task 0 typo fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 export default function getListStudents() {
-  return [{ id: 1, firstname: 'Guilluame', location: 'San Francisco' },
+  return [{ id: 1, firstname: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstname: 'James', location: 'Columbia' },
     { id: 5, firstname: 'Serena', location: 'San Francisco' },
   ];
