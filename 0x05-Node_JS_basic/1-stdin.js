cec096c4ab7b5a125ec69938e235bca52bfca391
nuel07 <yehanganep@gmail.com>
2022-05-31 06:15:47 -0700

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const msg = process.stdin.read();
+  if (msg !== null) {
+    process.stdout.write(`Your name is: ${msg}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
