32640b82af18f9eccb3d2d340543ab95e89ffc26
nuel07 <yehanganep@gmail.com>
2022-05-31 08:12:58 -0700

    task 3

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let content;
+  try {
+    content = fs.readFileSync(path).toString();
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  content = content.split('\n');
+  let students = content.filter((colmn) => colmn);
+  const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+  students = students.map((colmn) => colmn.split(','));
+  const fields = {};
+  for (const s in students) {
+    if (s !== 0) {
+      if (!fields[students[s][3]]) {
+	   fields[students[s][3]] = [];
+	   fields[students[s][3]].push(students[s][0]);
+      }
+    }
+  }
+  delete fields.field;
+
+  for (const k of Object.keys(fields)) {
+    console.log(`Number of students in ${k}: ${fields[k].length}, List: ${fields[k].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
